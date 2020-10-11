214b75fac6fe1b3b3e2c7e1d3dd8baecca57af29
MarinaAbdelMassih <mabdelmassih@panarab-media.com>
2020-10-11 17:09:41 +0200

    sliderService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
-import { HeaderModule } from './core/components/header/header.module';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SharedModule} from './shared/shared.module';
+import {HeaderModule} from './core/components/header/header.module';
 
-import { FooterComponent } from './core/components/footer/footer.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { AuthInterceptor } from './core/interceptors/auth.interceptor';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
+import {FooterComponent} from './core/components/footer/footer.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {AuthInterceptor} from './core/interceptors/auth.interceptor';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {CollapseModule} from 'ngx-bootstrap/collapse';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {ContactUsComponent} from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    
+
     FooterComponent,
-    
+
     AboutUsComponent,
-    
+
     ContactUsComponent,
   ],
   imports: [
@@ -31,9 +31,11 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
     BrowserAnimationsModule,
     SharedModule,
     HttpClientModule,
-     HeaderModule ,
+    HeaderModule,
     BsDropdownModule.forRoot(),
     CollapseModule.forRoot(),
+    HttpClientModule
+
   ],
   providers: [
     {
